62e3cf2b206a79ee369b8ac63a14fddb5bf52970
visualstudio <visualstudio@230-PC19>
2019-04-18 15:09:50 -0400

    y177

--- assgnmnt5/Program.cs
+++ assgnmnt5/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace assgnmnt5
+namespace FrequencyOfArray
 {
     class Program
     {
-        static void Main(string[] args)
+        public int[] inputArray = new int[10] { 3, 11, 9, 11, 4, 11, 5, 9, 2, 1 };
+
+        static void Main(string[] args) { new Program().Run(); }
+
+        public void Run() { this.InterateOverArray(); }
+
+        public void InterateOverArray()
         {
-            Console.WriteLine("Hello World!");
+            int output;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                output = inputArray[i];
+
+                Console.WriteLine(Convert.ToString(output));
+            }
         }
     }
 }
